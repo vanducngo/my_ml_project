a4685f38fcfc594a416f6b3d9d24b2cc02b75098
ngovd <vanducngo190@gmail.com>
2026-01-25 10:57:50 +0700

    F

--- processor/urls.py
+++ processor/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('label_customer/', views.new_transaction, name='label_customer'),
     path('retrain_all/', views.retrain_all, name='retrain_all'),
-    path('relable_all/', views.relable_all, name='relable_all'),
+    path('relabel_all/', views.relabel_all, name='relabel_all'),
 ]
--- processor/views.py
+++ processor/views.py
@@ -17,6 +17,20 @@ def new_transaction(request):
             status=status.HTTP_400_BAD_REQUEST
         )
 
+    customer_id = str(customer_id).strip()
+    try:
+        # Thử convert sang float trước để bắt trường hợp '12345.0'
+        float_val = float(customer_id)
+        # Kiểm tra xem nó có phải số nguyên không (vd: 123.0 là OK, 123.5 là Fail)
+        if not float_val.is_integer():
+            return {"status": "error", "message": "Customer ID must be an integer (e.g., 12345)"}
+        
+        # Nếu OK, chuẩn hóa về dạng số nguyên string (bỏ .0)
+        customer_id = str(int(float_val))
+    except ValueError:
+        # Nếu không thể convert sang float -> chắc chắn chứa ký tự chữ
+        return {"status": "error", "message": f"Invalid Customer ID format: '{customer_id}'. Must contains digits only."}
+
     # --- Code xử lý logic transaction ở đây ---
     print(f"Đang xử lý giao dịch cho khách hàng: {customer_id}")
     
@@ -43,10 +57,10 @@ def retrain_all(request):
 
 # API 3: retrain_classifier
 @api_view(['POST'])
-def relable_all(request):
-    print("Chỉ train lại Classifier...")
+def relabel_all(request):
+    print("relabel_all...")
     
     engine = RFMEngine()
-    result = engine.predict_customer()
+    result = engine.predict()
     
     return JsonResponse(result)
